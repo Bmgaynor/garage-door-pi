5ea00227c63e2d26b0b1ad073cfdbf5f8d3a031d
bradley gaynor <bradmgaynor@gmail.com>
2024-12-25 18:54:36 -0800

    feat: try onoff

--- cycle-test.js
+++ cycle-test.js
@@ -9,15 +9,16 @@
 //   }
 // }
 
-var gpio = require("rpi-gpio");
+const Gpio = require("onoff").Gpio; // Gpio class
+const led = new Gpio(26, "out"); // Export GPIO17 as an output
 
-gpio.setup(26, gpio.DIR_HIGH, write);
+// Toggle the state of the LED connected to GPIO17 every 200ms
+const iv = setInterval((_) => led.writeSync(led.readSync() ^ 1), 200);
 
-function write(err) {
-  if (err) throw err;
-  gpio.write(26, false, function (err) {
-    if (err) throw err;
-    console.log("Written to pin");
-  });
-}
+// Stop blinking the LED after 5 seconds
+setTimeout((_) => {
+  clearInterval(iv); // Stop blinking
+  led.unexport(); // Unexport GPIO and free resources
+}, 5000);
+
 // main();

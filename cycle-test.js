de6156417719578a1bf0b560a9e3864576608cf3
bradley gaynor <bradmgaynor@gmail.com>
2024-12-25 19:19:45 -0800

    feat: press button

--- cycle-test.js
+++ cycle-test.js
@@ -1,63 +1,14 @@
-// const { pressButton } = require("./door");
+const { pressButton } = require("./door");
 
-// async function main() {
-//   try {
-//     await pressButton();
-//   } catch (err) {
-//     console.log("error occurred");
-//     console.error(err);
-//   }
-// }
-
-const { exec } = require("child_process");
-
-const runCommand = (command) => {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve(stdout.trim());
-    });
-  });
-};
-
-const getPinStatus = async (pin) => {
-  const command = `raspi-gpio get ${pin}`;
+async function main() {
   try {
-    const result = await runCommand(command);
-    const parts = result.split("level=");
-    return parseInt(parts[1].charAt(0));
-  } catch (error) {
-    console.error(error);
-    return null;
+    console.log("starting");
+    await pressButton();
+    console.log("complete");
+  } catch (err) {
+    console.log("error occurred");
+    console.error(err);
   }
-};
+}
 
-// then later, when I'm doing something - make sure its an async something so I can await getPinStatus
-const main = async () => {
-  const LED = 26;
-  const pinStatus = await getPinStatus(LED);
-  console.log(pinStatus);
-  await runCommand(``);
-  await runCommand(`raspi-gpio set ${LED} op`); // set gpio 5 to output
-  await runCommand(`raspi-gpio set ${LED} dh`); // set gpio 5 high
-  // await runCommand(`raspi-gpio set ${LED} dl`); // set gpio 5 low
-
-  // pinStatus will be 1 or 0
-};
-
-// const Gpio = require("onoff").Gpio; // Gpio class
-// const led = new Gpio(26, "out"); // Export GPIO17 as an output
-
-// // Toggle the state of the LED connected to GPIO17 every 200ms
-// const iv = setInterval((_) => led.writeSync(led.readSync() ^ 1), 200);
-
-// // Stop blinking the LED after 5 seconds
-// setTimeout((_) => {
-//   clearInterval(iv); // Stop blinking
-//   led.unexport(); // Unexport GPIO and free resources
-// }, 5000);
-
-// main();
+main();
--- door.js
+++ door.js
@@ -1,5 +1,17 @@
-const config = require("./config");
-var gpiop = require("rpi-gpio").promise;
+// const config = require("./config");
+const { exec } = require("child_process");
+
+const runCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(stdout.trim());
+    });
+  });
+};
 
 async function wait(ms) {
   return new Promise((resolve) => {
@@ -9,30 +21,29 @@ async function wait(ms) {
   });
 }
 
-async function pressButton() {
+const getPinStatus = async (pin) => {
+  const command = `raspi-gpio get ${pin}`;
   try {
-    gpiop
-      .setup(26, "out")
-      .then(() => {
-        return gpiop.write(26, true);
-      })
-      .catch((err) => {
-        console.log("Error: ", err.toString());
-      });
-    // await gpiop.setup(config.GARAGE_PIN, gpiop.DIR_OUT);
-    // console.log("set up complete");
-    // await gpiop.write(config.GARAGE_PIN, true);
-    // console.log("it on");
-    // // await writePin(config.GARAGE_PIN, config.RELAY_ON);
-    // await wait(config.RELAY_TIMEOUT);
-
-    // await gpiop.write(config.GARAGE_PIN, false);
-    // console.log("it off");
-  } catch (err) {
-    console.error(err);
+    const result = await runCommand(command);
+    const parts = result.split("level=");
+    return parseInt(parts[1].charAt(0));
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-}
+};
 
+// then later, when I'm doing something - make sure its an async something so I can await getPinStatus
+const pressButton = async () => {
+  const LED = 26;
+  const pinStatus = await getPinStatus(LED);
+  console.log(pinStatus);
+
+  await runCommand(`raspi-gpio set ${LED} op`);
+  await runCommand(`raspi-gpio set ${LED} dh`);
+  await wait(500);
+  await runCommand(`raspi-gpio set ${LED} dl`);
+};
 module.exports = {
   pressButton,
 };
